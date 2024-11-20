c99f51d40f4e0b672130543b7259ec65469a1fcb
isaacbatista26 <isaacbatista2266@gmail.com>
2024-11-20 18:43:00 -0300

    feat: add new error message for lesson requests

--- src/enum/EnumErrorMessages.ts
+++ src/enum/EnumErrorMessages.ts
@@ -47,6 +47,7 @@ export enum EnumErrorMessages {
   SUBJECT_ID_INVALID = 'O Id da matéria deve ser um número.',
 
   // Lesson errors
+  ANY_LESSON_REQUEST_FOUND = 'Nenhuma aula encontrada para o seu perfil.',
   LESSON_REQUEST_NOT_FOUND = 'Aula não encontrada.',
   EXISTING_LESSON = 'Já existe uma aula agendada para o aluno nesse horário: ${date}',
   REASON_REQUIRED = 'Motivo da aula é obrigatório.',
