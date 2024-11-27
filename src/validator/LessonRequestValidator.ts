cab7b90d7c6749a0d109ba943a3a1ac590201475
isaacbatista26 <isaacbatista2266@gmail.com>
2024-11-27 10:48:02 -0300

    feat: add validation for lesson request query parameters

--- src/validator/LessonRequestValidator.ts
+++ src/validator/LessonRequestValidator.ts
@@ -1,4 +1,4 @@
-import { body } from 'express-validator';
+import { body, query } from 'express-validator';
 import { EnumReasonName } from '../enum/EnumReasonName';
 import { BaseValidator } from './BaseValidator';
 import { LessonRequestRepository } from '../repository/LessonRequestRepository';
@@ -159,6 +159,30 @@ export class LessonRequestValidator {
         .withMessage(EnumErrorMessages.ADDITIONAL_INFO_STRING)
         .isLength({ max: 200 })
         .withMessage(EnumErrorMessages.ADDITIONAL_INFO_LENGTH)
+    ]);
+  }
+  static getLessonRequests() {
+    return BaseValidator.validationList([
+      query('id')
+        .optional()
+        .isInt({ min: 1 })
+        .withMessage(EnumErrorMessages.TUTOR_ID_INVALID),
+      query('page')
+        .optional()
+        .isInt({ min: 1 })
+        .withMessage(EnumErrorMessages.INVALID_PAGE),
+      query('size')
+        .optional()
+        .isInt({ min: 1 })
+        .withMessage(EnumErrorMessages.INVALID_SIZE),
+      query('order')
+        .optional()
+        .isIn(['ASC', 'DESC'])
+        .withMessage(EnumErrorMessages.ORDER_INVALID),
+      query('orderBy')
+        .optional()
+        .isString()
+        .withMessage(EnumErrorMessages.ORDER_BY_INVALID)
     ]);
   }
 }
