7ec10950e3eabc1130bb8cf581c95febe6145d43
isaacbatista26 <isaacbatista2266@gmail.com>
2024-11-27 10:01:14 -0300

    feat: hide username, password, and salt fields from query results

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -5,16 +5,22 @@ export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ type: 'varchar', length: 255, unique: true, nullable: false })
+  @Column({
+    type: 'varchar',
+    length: 255,
+    unique: true,
+    nullable: false,
+    select: false
+  })
   username: string;
 
   @Column({ type: 'varchar', length: 255, unique: true, nullable: false })
   email: string;
 
-  @Column({ type: 'varchar', length: 255, nullable: false })
+  @Column({ type: 'varchar', length: 255, nullable: false, select: false })
   password: string;
 
-  @Column({ type: 'varchar', length: 255, nullable: false })
+  @Column({ type: 'varchar', length: 255, nullable: false, select: false })
   salt: string;
 
   @Column({ type: 'varchar', length: 255, nullable: false })
