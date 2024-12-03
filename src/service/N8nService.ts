050c1f72b001732eaa889a36ea0545d1cc2f8b64
isaacbatista26 <isaacbatista2266@gmail.com>
2024-12-02 21:45:28 -0300

    refactor: rename n8nService file to N8nService and update URL

--- src/service/N8nService.ts
+++ src/service/N8nService.ts
@@ -6,7 +6,8 @@ import { handleError } from '../utils/ErrorHandler';
 export class N8nService {
   static async triggerGoogleMeetWebhook(lessonRequest: LessonRequest) {
     try {
-      const n8nResponse = await axios.post(`${process.env.N8N_MEET_URL}`, lessonRequest);
+      const n8nResponse = await axios.post(`http://n8n:5678/webhook/google-meet/`, lessonRequest);
+      console.log('\n\n\n\n\n\nn8nResponse', n8nResponse.status);
       // Do que exatamente eu preciso em LessonRequest para enviar para o n8n?
       return n8nResponse.data;
     } catch (error) {
