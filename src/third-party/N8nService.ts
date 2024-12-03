50798f891210003133770316d01897cdd0bd474a
isaacbatista26 <isaacbatista2266@gmail.com>
2024-12-03 18:30:38 -0300

    feat: implement saveMeetUrl after webhook response

--- src/third-party/N8nService.ts
+++ src/third-party/N8nService.ts
@@ -2,13 +2,15 @@ import axios from 'axios';
 import { LessonRequest } from 'entity/LessonRequest';
 import { AppError } from '../error/AppError';
 import { handleError } from '../utils/ErrorHandler';
+import { LessonRequestRepository } from '../repository/LessonRequestRepository';
 
 export class N8nService {
   static async triggerGoogleMeetWebhook(lessonRequest: LessonRequest) {
     try {
-      const n8nResponse = await axios.post(`http://n8n:5678/webhook/google-meet/`, lessonRequest);
-      console.log('\n\n\n\n\n\nn8nResponse', n8nResponse.status);
-      // Do que exatamente eu preciso em LessonRequest para enviar para o n8n?
+      const n8nResponse = await axios.post(`http://n8n:5678/webhook/google-meet`, lessonRequest);
+      if (n8nResponse.data.hangoutLink) {
+        await LessonRequestRepository.saveMeetUrl(lessonRequest.classId, n8nResponse.data.hangoutLink);
+      }
       return n8nResponse.data;
     } catch (error) {
       const { statusCode, message } = handleError(error);
