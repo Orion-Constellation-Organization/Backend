671806ed89ebd408616b01909bfc74934245f89d
isaacbatista26 <isaacbatista2266@gmail.com>
2024-12-03 21:45:40 -0300

    refactor: update N8nService to use environment variable and simplify error handling

--- src/third-party/N8nService.ts
+++ src/third-party/N8nService.ts
@@ -1,20 +1,16 @@
 import axios from 'axios';
 import { LessonRequest } from 'entity/LessonRequest';
-import { AppError } from '../error/AppError';
-import { handleError } from '../utils/ErrorHandler';
 import { LessonRequestRepository } from '../repository/LessonRequestRepository';
 
 export class N8nService {
   static async triggerGoogleMeetWebhook(lessonRequest: LessonRequest) {
     try {
-      const n8nResponse = await axios.post(`http://n8n:5678/webhook/google-meet`, lessonRequest);
+      const n8nResponse = await axios.post(process.env.N8N_MEET_URL, lessonRequest);
       if (n8nResponse.data.hangoutLink) {
         await LessonRequestRepository.saveMeetUrl(lessonRequest.classId, n8nResponse.data.hangoutLink);
       }
-      return n8nResponse.data;
     } catch (error) {
-      const { statusCode, message } = handleError(error);
-      throw new AppError(message, statusCode);
+      return null;
     }
   }
 }
